9a07afd1fc27d0c6a8162188860575c343848124
robosnob <enzo.mail@gmail.com>
2021-05-05 14:40:50 +0000

    dag files for airflow

--- dags/fetch_data.py
+++ dags/fetch_data.py
@@ -2,7 +2,6 @@ from datetime import timedelta
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
 
 args = {'owner': 'airflow'}
